a44bfe4c4f529a574fee92ef28862513e8528e6b
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-12-10 15:39:46 +0530

    Update jstar2.js

--- api/jstar2.js
+++ api/jstar2.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   }
 
   // Otherwise → treat as IPTV / player → return the M3U content directly
-  const m3uUrl = 'https://raw.githubusercontent.com/alex8875/m3u/main/jstar.m3u';
+  const m3uUrl = 'https://jtv.pfy.workers.dev';
 
   try {
     const response = await fetch(m3uUrl);
