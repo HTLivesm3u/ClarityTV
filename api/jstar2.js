1b0cbd62e1287f2f3e8f91838c1ce6b5ff74131c
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-12-19 17:30:00 +0530

    Update jstar2.js

--- api/jstar2.js
+++ api/jstar2.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   }
 
   // Otherwise → treat as IPTV / player → return the M3U content directly
-  const m3uUrl = 'https://jtv.pfy.workers.dev';
+  const m3uUrl = 'http://activefrance.net:80/live/12445677/0BC3C9C05265/1090636.ts';
 
   try {
     const response = await fetch(m3uUrl);
