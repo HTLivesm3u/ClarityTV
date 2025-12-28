9680fa545c1b3dc06b921b03ee2da8aa07043eed
unkown_4777 <169054920+HTLivesm3u@users.noreply.github.com>
2025-12-28 09:42:18 +0530

    Update jstar2.js

--- api/jstar2.js
+++ api/jstar2.js
@@ -23,7 +23,7 @@ export default async function handler(req, res) {
   }
 
   // Otherwise → treat as IPTV / player → return the M3U content directly
-  const m3uUrl = 'https://jtv.pfy.workers.dev/';
+  const m3uUrl = 'https://jtvplus.streamflex.workers.dev/JTVPLUSONLY.m3u';
 
   try {
     const response = await fetch(m3uUrl);
